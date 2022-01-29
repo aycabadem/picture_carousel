840d9021fc03b44a7350b6f470989e4b79b5520f
aycabadem <aycabadem12@gmail.com>
2022-01-29 13:20:07 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _imagePaths = [
+    'images/day1.jpeg',
+    'images/day2.jpeg',
+    'images/day3.jpeg',
+    'images/day4.jpeg',
+    'images/day5.jpeg',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Picture Carousel'),
+        ),
+        body: Center(
+          child: SafeArea(
+            child: CarouselSlider(
+                options: CarouselOptions(autoPlay: true),
+                items: _imagePaths.map((imagePath) {
+                  return Builder(builder: (context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 4),
+                        child: Image.asset(
+                          imagePath,
+                          fit: BoxFit.cover,
+                        ));
+                  });
+                }).toList()),
+          ),
+        ),
+      ),
+    );
+  }
+}
